b010cfd14fe3274231973c564551537c867d340b
Alejandro Santorum <alejandro.santorum@gmail.com>
2019-04-17 00:23:17 +0200

    generate_board + scrape_main functions implemented. Won boards should be truncated or preprocessed in the future

--- c4india.py
+++ c4india.py
@@ -1,118 +1,93 @@
-#######################################################
-# USELESS IMPORTS
-#######################################################
-#import requests
-#import urllib.request
-#from urllib.request import Request,urlopen
-#from bs4 import BeautifulSoup
-#from lxml import html
-#import lxml
-#from lxml import etree
-#import sys
-#import bond as bd
-#from datetime import datetime
-#######################################################
 import time
+from random import randint
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+SLEEP_TIME = 0.65
+MAIN_URL = 'https://connect4.gamesolver.org/?pos='
+
+PIECES = ["1", "1", "1", "1", "1", "1",
+		  "2", "2", "2", "2", "2", "2",
+		  "3", "3", "3", "3", "3", "3",
+		  "4", "4", "4", "4", "4", "4",
+		  "5", "5", "5", "5", "5", "5",
+		  "6", "6", "6", "6", "6", "6",
+		  "7", "7", "7", "7", "7", "7"]
+
+MAX_PIECES = 41
+MAX_PLAYS = 35
+MIN_PLAYS = 5
+
+
+def generate_board():
+	board = ""
+	n_plays = randint(MIN_PLAYS, MAX_PLAYS)
+	for i in range(n_plays):
+		pos = randint(0,MAX_PIECES-i)
+		board += PIECES[pos]
+	return board
+
+
+def scrape_main(n_examples):
+	first_pos = generate_board()
+	first_url = MAIN_URL + first_pos
+
+	#######################################
+	# delete print later
+	print(first_pos)
+	#######################################
+
+	# Initializing webdriver (Chrome in this case)
+	driver = webdriver.Chrome()
+
+	# Opening it for the first time
+	driver.get(first_url)
+
+	try:
+		for i in range(n_examples):
+			# Opening a new tab to use it later
+			next_tab = "tab"+str(i+1)
+			new_tab_command = "window.open('about:blank', \'"+next_tab+"\');"
+			driver.execute_script(new_tab_command)
+
+			# Get button you are going to click by its id ( also you could us find_element_by_css_selector to get element by css selector)
+			button_element = driver.find_element_by_id('hide_solution_div')
+
+			# Getting points
+			time.sleep(SLEEP_TIME)
+			for j in range(0,7):
+				el = driver.find_element_by_xpath('//*[@id="sol' + str(j) + '"]')
+				#######################################
+				# delete print later
+				print(el.get_attribute('innerHTML'))
+
+			# Closing current tab
+			driver.close()
+
+			# Switching tabs
+			driver.switch_to.window(next_tab)
+
+			# Random URL generator soon...
+			board = generate_board()
+			#######################################
+			# delete print later
+			print(board)
+			#######################################
+			next_url = MAIN_URL+board
+			driver.get(next_url)
+
+	except Exception as err:
+		print("Something went wrong at iteration: "+str(i)+" ==> " + str(err))
+	finally:
+		driver.quit()
+		return
+
+
 M = 10
-SLEEP_TIME = 0.6
-
-pos = 452435213434534
-first_url = 'https://connect4.gamesolver.org/?pos=' + str(pos)
+if __name__ == "__main__":
+	scrape_main(M)
 
 
-# Initializing webdriver (Chrome in this case)
-driver = webdriver.Chrome()
-
-# Opening it for the first time
-driver.get(first_url)
-
-try:
-	for i in range(M):
-		# Opening a new tab to use it later
-		new_tab_command = "window.open('about:blank', 'tab"+str(i+1)+"\');"
-		driver.execute_script(new_tab_command)
-
-		# Get button you are going to click by its id ( also you could us find_element_by_css_selector to get element by css selector)
-		button_element = driver.find_element_by_id('hide_solution_div')
-
-		# Getting points
-		time.sleep(SLEEP_TIME)
-		for j in range(0,6):
-			el = driver.find_element_by_xpath('//*[@id="sol' + str(j) + '"]')
-			print(el.get_attribute('innerHTML'))
-
-		# Closing current tab
-		driver.close()
-
-		# Switching tabs
-		next_tab = "tab"+str(i+1)
-		driver.switch_to.window(next_tab)
-
-		# Random URL generator soon...
-		driver.get(first_url)
-
-except Exception as err:
-	print("Something went wrong at iteration: "+str(i)+" ==> " + str(err))
-finally:
-	driver.quit()
-
-
-
-###############################################################################
-# FIRST SCRATCH
-
-#driver = webdriver.Chrome()
-#driver.get("http://www.google.com/")
-# Go to your page url
-#try:
-#	driver.get(url)
-#	driver.execute_script("window.open('about:blank', 'tab2');")
-	# Get button you are going to click by its id ( also you could us find_element_by_css_selector to get element by css selector)
-#	button_element = driver.find_element_by_id('hide_solution_div')
-#	button_element.click()
-#	time.sleep(SLEEP_TIME)
-#	for i in range(0,6):
-#		el = driver.find_element_by_xpath('//*[@id="sol' + str(i) + '"]')
-#		print(el.get_attribute('innerHTML'))
-	#driver.execute_script("window.close('');")
-#	driver.close()
-#except Exception as e:
-#	print(e)
-#finally:
-#	driver.quit()
-
-
-###############################################################################
-# USELESS CODE (?)
-
-#lxml
-'''
-page = requests.get(url)
-page2 = page = requests.get(url,json)
-'''
-#jresponse = page
-#print(jresponse.text)
-'''
-tree = html.fromstring(page.content)
-#el = tree.xpath('/div#sol0.solution.col0')
-#print(lxml.html.tostring(el[0]))
-for i in range(0,6):
-	element = tree.xpath('//*[@id="sol' + str(i) +'"]/text()')
-	print(element)
-
-#beautifulsoup
-
-
-from bs4 import Comment
-
-html = BeautifulSoup(page.content, 'html.parser')
-msgDiv = html.find_all('div')
-for obj in msgDiv:
-	print(obj)'''
-###############################################################################
