7a40e07c58390f9d98e975b36d85023e28181842
Alejandro Santorum <alejandro.santorum@gmail.com>
2019-04-19 14:23:49 +0200

    separated xdata and ydata

--- fitting.py
+++ fitting.py
@@ -1,7 +1,8 @@
 import time
 
-def read_xdata_from_file(filename, sep):
+def read_data_from_file(filename, sep):
     xdata = []
+    ydata = []
     f = open(filename, "r")
     line = f.readline()
     while line:
@@ -9,10 +10,11 @@ def read_xdata_from_file(filename, sep):
         array = line.split(sep)
         for elem in array:
             aux.append(float(elem))
-        xdata.append(aux)
+        xdata.append(aux[:-1])
+        ydata.append(aux[-1])
         line = f.readline()
     f.close()
-    return xdata
+    return xdata, ydata
 
 
 FILENAME = "35K.csv"
@@ -20,8 +22,10 @@ SEP = ", "
 if __name__ == "__main__":
     print("READING DATA.......")
     start = time.time()
-    xdata = read_xdata_from_file(FILENAME, SEP)
+    xdata, ydata = read_data_from_file(FILENAME, SEP)
     fin = time.time()
     print("FINISHED THE READING.......")
     print(xdata)
+    print(ydata)
+    print("Length: ", len(ydata))
     print("Time => ", fin - start)
