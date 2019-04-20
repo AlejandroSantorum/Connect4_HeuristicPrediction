9d44b161fdd7e76c4ab068de3b6846140a7cf498
Alejandro Santorum <alejandro.santorum@gmail.com>
2019-04-20 17:22:09 +0200

    Improved fitting (using sklearn.linearModel) + added #iteration at scraping

--- fitting.py
+++ fitting.py
@@ -1,6 +1,9 @@
 import time
 import numpy as np
 from scipy.optimize import curve_fit
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -28,37 +31,110 @@ def expression_func1(X, th0, th1, th2, th3, th4, th5, th6, th7, th8, th9, th10, 
     return res
 
 
-def test(xdata_test, ydata_test, popt, tol):
+def my_test(xdata_test, ydata_test, popt, intercepter, tol):
     good = 0
-    length = len(xdata_test)
-    for i in range(length):
-        if abs(expression_func1(xdata_test[i], *popt) - ydata[i]) < tol:
+    m, n_feat = xdata_test.shape
+    for i in range(m):
+        pred = 0
+        for j in range(n_feat-1):
+            pred += popt[j]*xdata_test[i][j+1]
+        pred += intercepter
+        print("pred:", pred)
+        print("ydata_test:", ydata_test[i])
+        if pred > tol and ydata_test[i] > tol:
             good += 1
+        elif pred < -tol and ydata_test[i] < -tol:
+            good += 1
+        elif (pred < tol and pred > -tol) and (ydata_test[i] < tol and ydata_test[i] > -tol):
+            good += 1
+        else:
+            continue
+    return (good/m)*100
+
+def my_test2(y_pred, y_test, tol):
+    good = 0
+    length = len(y_pred)
+    for i in range(length):
+        if y_pred[i]>tol and y_test[i]>tol:
+            good += 1
+        elif y_pred[i]<-tol and y_test[i]<-tol:
+            good += 1
+        elif (y_pred[i]<tol and y_pred[i]>-tol) and (y_test[i]<tol and y_test[i]>-tol):
+            good += 1
+        else:
+            continue
     return (good/length)*100
 
 
-FILENAME = "35K.csv"
-SEP = ", "
-TRAIN_TEST_TH = 30000
-if __name__ == "__main__":
+def linear_regression(filename, train_test_h, feat_degree):
     # Reading
-    data = np.loadtxt(FILENAME, delimiter=SEP, usecols=range(11))
-    ydata = np.loadtxt(FILENAME, usecols=11)
+    data = np.loadtxt(filename, delimiter=SEP, usecols=range(Y_COL))
+    ydata = np.loadtxt(filename, usecols=Y_COL)
 
-    # Adding one column of 1's at the beginning
-    ones_col = np.ones((data.shape[0], 1))
-    xdata = np.append(ones_col, data, 1)
+    # Feature preparation
+    poly_feat = PolynomialFeatures(feat_degree)
+    xdata = poly_feat.fit_transform(data)
+
+    # Train and test set
+    xdata_train = xdata[:train_test_h]
+    xdata_test = xdata[train_test_h:]
+    ydata_train = ydata[:train_test_h]
+    ydata_test = ydata[train_test_h:]
+
+    # Linear regression (actually polynomial regression)
+    lin_reg = LinearRegression()
+    # Fitting
+    lin_reg.fit(xdata_train, ydata_train)
+    # Predicting
+    y_pred = lin_reg.predict(xdata_test)
+
+    print("YPRED:")
+    print(y_pred)
+
+    # The coefficients
+    print('Coefficients: \n', lin_reg.coef_)
+    # The mean squared error
+    print("Mean squared error: %.2f" % mean_squared_error(ydata_test, y_pred))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score: %.2f' % r2_score(ydata_test, y_pred))
+
+    print("My test: ", my_test2(y_pred, ydata_test, 1))
+
+
+def curve_fitting(filename, train_test_h, feat_degree):
+    # Reading
+    data = np.loadtxt(FILENAME, delimiter=SEP, usecols=range(Y_COL))
+    ydata = np.loadtxt(FILENAME, usecols=Y_COL)
+
+    # Feature preparation
+    poly_feat = PolynomialFeatures(FEAT_DEGREE)
+    xdata = poly_feat.fit_transform(data)
+
+    # Train and test set
     xdata_train = xdata[:TRAIN_TEST_TH]
     xdata_test = xdata[TRAIN_TEST_TH:]
     ydata_train = ydata[:TRAIN_TEST_TH]
     ydata_test = ydata[TRAIN_TEST_TH:]
-    xdata_train = np.transpose(xdata_train)
-    print("xdata shape: ", xdata_train.shape)
-    print("ydata shape: ", ydata_train.shape)
+    #xdata_train = np.transpose(xdata_train) # <--- curve_fit 
 
-    popt, pcov = curve_fit(expression_func1, xdata_train, ydata_train)
-    print("Popt: ", popt)
-    perr = np.sqrt(np.diag(pcov))
-    print("perr: ", perr)
 
-    print(test(xdata_test, ydata_test, popt, 3))
+    #popt, pcov = curve_fit(expression_func1, xdata_train, ydata_train)
+    #print("Popt: ", popt)
+    #perr = np.sqrt(np.diag(pcov))
+    #print("perr: ", perr)
+
+    #print(test(xdata_test, ydata_test, popt, 3))
+
+
+
+
+FILENAME = "190K.csv"
+SEP = ", "
+TRAIN_TEST_TH = 160000
+Y_COL = 11
+FEAT_DEGREE = 2
+
+if __name__ == "__main__":
+
+    linear_regression(FILENAME, TRAIN_TEST_TH, FEAT_DEGREE)
+
